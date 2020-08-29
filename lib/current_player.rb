4eda2e775fe03ef65217e666838d5e2719a550ec
Catherine Gloria <catherinegloria@gmail.com>
2020-08-29 22:33:19 +0000

    Automatically backed up by Learn

--- lib/current_player.rb
+++ lib/current_player.rb
@@ -1,11 +1,11 @@
 def turn_count(board)
   turns_array = []
   board.each do |position|
-
   if board[location] != " " && board[location] != ""
     turns_array.push(board[location])  #add to turn count
   end
   #returns number of turns played
+  turns.size
 end
 
 def current_player(board)
