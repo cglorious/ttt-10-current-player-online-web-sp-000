12d30030afb15029356c7de23a0de63bb3cf3aac
Catherine Gloria <catherinegloria@gmail.com>
2020-08-29 22:28:21 +0000

    Automatically backed up by Learn

--- lib/current_player.rb
+++ lib/current_player.rb
@@ -1,10 +1,12 @@
 def turn_count(board)
   board.each do |position|
-    
+
   end
   #returns number of turns played
 end
 
-def current_player
-
+def current_player(board)
+ if board.length % 2 = 0
+    true
+ end
 end
